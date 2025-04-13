463f7dbca96eea5a2239cfd82aeddeebdc487f06
WatsonWaswa <watsonliwa@yahoo.com>
2025-04-13 16:39:19 +0300

    Bookstore Database Assignment - PLP - SHEKEWA GROUP

---
+++ bookstore.sql
@@ -0,0 +1,17 @@
+/* 
+
+----MEMBERS----
+
+LIWA WATSON - watsonliwa@yahoo.com
+SHEKINAH NYAMBURA - nshekinah7@gmail.com
+KENNEDY MWANIKI - kennedymwaniki089@gmail.com
+
+*/
+
+/* 
+
+git branch -M main 
+git remote add origin https://github.com/WatsonWaswa/BOOKSTORE_DATABASE_PLP---GROUP-SHEKEWA.git 
+
+*/
+
